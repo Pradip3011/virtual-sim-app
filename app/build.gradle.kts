4425bf3ae13248a9edae68beb0f2153a72a9ffdd
PJ AGENTIC SENTINEL / #KiwiBaby <116477313+Pradip3011@users.noreply.github.com>
2026-06-04 03:58:49 +1200

    Update Gradle plugin versions in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
 }
 
 android {
