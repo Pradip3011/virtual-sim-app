764e1b5cffebc5e307abf29de90925155c15915b
PJ AGENTIC SENTINEL / #KiwiBaby <116477313+Pradip3011@users.noreply.github.com>
2026-06-04 05:11:07 +1200

    Refactor SimForegroundService for improved readability

--- app/src/main/java/com/omnitest/virtual_sim/service/SimForegroundService.kt
+++ app/src/main/java/com/omnitest/virtual_sim/service/SimForegroundService.kt
@@ -11,8 +11,8 @@ import androidx.core.app.NotificationCompat
 import com.omnitest.virtual_sim.model.StreamRegistration
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.websocket.WebSockets
-import io.ktor.client.plugins.websocket.webSocket
+import io.ktor.client.plugins.websocket.WebSocket.*
+import io.ktor.client.plugins.websocket.webSocket.
 import io.ktor.websocket.Frame
 import io.ktor.websocket.readText
 import kotlinx.coroutines.CoroutineScope
